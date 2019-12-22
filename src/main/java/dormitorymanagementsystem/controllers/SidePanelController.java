dd2dfd708bcf7d576b3ddcbf5d7d59536eb08ff3
M1nist3r <andriztan1@gmail.com>
2019-12-22 07:24:49 +0100

    Nav menu

---
+++ src/main/java/dormitorymanagementsystem/controllers/SidePanelController.java
@@ -0,0 +1,98 @@
+package dormitorymanagementsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class SidePanelController {
+
+
+    @FXML
+    private Button addResidentButton;
+
+    @FXML
+    private Button listOfResidentButton;
+
+    @FXML
+    private Button listOfRoomButton;
+
+    @FXML
+    private Button paymentDetailsList;
+
+    @FXML
+    private Button archiveListButton;
+
+    @FXML
+    private Button settingsListButton;
+
+    private Stage primaryStage;
+
+    void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public void initialize() {
+        Image imageButtom = new Image(getClass().getResourceAsStream("/img/baseline_person_add_black_18dp.png"));
+        addResidentButton.setGraphic(new ImageView(imageButtom));
+        imageButtom = new Image(getClass().getResourceAsStream("/img/baseline_group_black_18dp.png"));
+        listOfResidentButton.setGraphic(new ImageView(imageButtom));
+        imageButtom = new Image(getClass().getResourceAsStream("/img/baseline_business_black_18dp.png"));
+        listOfRoomButton.setGraphic(new ImageView(imageButtom));
+        imageButtom = new Image(getClass().getResourceAsStream("/img/baseline_payment_black_18dp.png"));
+        paymentDetailsList.setGraphic(new ImageView(imageButtom));
+        imageButtom = new Image(getClass().getResourceAsStream("/img/baseline_archive_black_18dp.png"));
+        archiveListButton.setGraphic(new ImageView(imageButtom));
+    }
+
+    @FXML
+    void addResident(MouseEvent event) {
+        //changeScene("/fxml/");
+    }
+
+    @FXML
+    void showArchive(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showListOfResident(MouseEvent event) {
+        try {
+//            changeScene("/fxml/ResidentList.fxml");
+            changeScene();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void showListOfRooms(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showPayments(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showSettings(MouseEvent event) {
+
+    }
+
+    private void changeScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/ResidentList.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Dormitory management system");
+        primaryStage.setHeight(720);
+        primaryStage.setWidth(1280);
+        primaryStage.getScene().setRoot(root);
+    }
+}
